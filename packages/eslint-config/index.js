1a9048d2e31dce4c943bb17c2ab5d51db2705c5c
RafaelHuszcza <rafaelhuszcza@gmail.com>
2023-05-06 19:20:17 -0300

    Todas alterações

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ['@rocketseat/eslint-config/react'],
+  rules: {
+    "camelcase": "off",
+    "no-useless-constructor": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        "bracketSpacing": "off"
+      }
+    ]
+  }
+
 }
